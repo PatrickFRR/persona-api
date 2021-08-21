c7b5d56559ad4eeb5bfa0c83a67dbdb0c6cd4025
PatrickFRR <patrick_ribeiro@ymail.com>
2021-08-21 20:57:58 -0300

    add classes e test

--- src/main/java/com/github/patrickfrr/personapi/PersonapiAplication.java
+++ src/main/java/com/github/patrickfrr/personapi/PersonapiAplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PersonapiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PersonapiApplication.class, args);
-	}
-
+public class PersonapiAplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PersonapiApplication.class, args);
+    }
 }
