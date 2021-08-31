3fd6ad5ca6b4a44b045fce08350a92343d087b1e
PatrickFRR <patrick_ribeiro@ymail.com>
2021-08-30 23:02:57 -0300

    add dependency file pom.xml

--- src/main/java/com/github/patrickfrr/personapi/PersonapiApplication.java
+++ src/main/java/com/github/patrickfrr/personapi/PersonapiApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PersonapiAplication {
+public class PersonapiApplication {
     public static void main(String[] args) {
         SpringApplication.run(PersonapiApplication.class, args);
     }
+
 }
